ac9f8248771b90949ed2f3503d586028d36ee945
David <david.norton@cgi.com>
2017-07-20 09:47:32 +0100

    section 7 setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './servers/server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { CockpitComponent } from './cockpit/cockpit.component';
-import { ServerElementComponent } from './server-element/server-element.component';
+import { ServerComponent } from './section3/servers/server/server.component';
+import { ServersComponent } from './section3/servers/servers.component';
+import { HeaderComponent } from './shopping/header/header.component';
+import { RecipesComponent } from './shopping/recipes/recipes.component';
+import { RecipeListComponent } from './shopping/recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './shopping/recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './shopping/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping/shopping-list/shopping-edit/shopping-edit.component';
+import { CockpitComponent } from './section5/cockpit/cockpit.component';
+import { ServerElementComponent } from './section5/server-element/server-element.component';
 import { Assignment4Component } from './assignment4/assignment4.component';
 import { GameControlComponent } from './Assignment4/game-control/game-control.component';
 import { OddComponent } from './Assignment4/odd/odd.component';
 import { EvenComponent } from './Assignment4/even/even.component';
+import { DirectivesComponent } from './section7/directives/directives.component';
 
 @NgModule({
   declarations: [
@@ -37,7 +38,8 @@ import { EvenComponent } from './Assignment4/even/even.component';
     Assignment4Component,
     GameControlComponent,
     OddComponent,
-    EvenComponent
+    EvenComponent,
+    DirectivesComponent
   ],
   imports: [
     BrowserModule,
