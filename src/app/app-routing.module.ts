74cd298e74cd60856650487d6d968b2e73f0752d
David <david.norton@cgi.com>
2017-08-02 09:21:40 +0100

    143. Setting Up Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
+import { RecipesComponent } from './shopping/recipes/recipes.component';
 import { ServerResolver } from './section11/servers/server/server-resolver.service';
 import { ErrorPageComponent } from './section11/error-page/error-page.component';
 import { CanDeactivateGuard } from './section11/servers/edit-server/can-deactivate-guard.service';
@@ -14,25 +16,30 @@ import { NgModule } from '@angular/core';
 
 
 const appRoutes: Routes = [
-    { path: '', component: HomeComponent },
-    {
-        path: 'users', component: UsersComponent, children: [
-            { path: ':id:/:name', component: UserComponent },
-        ]
-    }, {
-        path: 'servers',
-        // canActivate: [AuthGuard],
-        canActivateChild: [AuthGuard],
-        component: ServersRoutingComponent,
-        children: [
-            { path: ':id', component: ServerRoutingComponent, resolve: {server: ServerResolver} },
-            { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
-        ]
-    },
-    // { path: 'not-found', component: PageNotFoundComponent },
-    { path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found!'} },
-    { path: '**', redirectTo: '/not-found' } // Ensure this is the last path
-    // { path: '', redirectTo: '/somewhere-else', pathMatch: 'full' } // Redirect if the path is empty to /somewhere-else
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+    { path: 'recipes', component: RecipesComponent },
+    { path: 'shopping-list', component: ShoppingListComponent },
+
+    // Section 11
+    // { path: '', component: HomeComponent },
+    // {
+    //     path: 'users', component: UsersComponent, children: [
+    //         { path: ':id:/:name', component: UserComponent },
+    //     ]
+    // }, {
+    //     path: 'servers',
+    //     // canActivate: [AuthGuard],
+    //     canActivateChild: [AuthGuard],
+    //     component: ServersRoutingComponent,
+    //     children: [
+    //         { path: ':id', component: ServerRoutingComponent, resolve: {server: ServerResolver} },
+    //         { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
+    //     ]
+    // },
+    // // { path: 'not-found', component: PageNotFoundComponent },
+    // { path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found!'} },
+    // { path: '**', redirectTo: '/not-found' } // Ensure this is the last path
+    // // { path: '', redirectTo: '/somewhere-else', pathMatch: 'full' } // Redirect if the path is empty to /somewhere-else
 ]
 
 @NgModule({
