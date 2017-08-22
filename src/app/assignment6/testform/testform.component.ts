b53ce518d52ab861a66a2223e802d049d268edb6
David <david.norton@cgi.com>
2017-08-22 16:02:13 +0100

    Assignment 6 submission

--- src/app/assignment6/testform/testform.component.ts
+++ src/app/assignment6/testform/testform.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-testform',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./testform.component.css']
 })
 export class TestformComponent implements OnInit {
+  @ViewChild('demoform') signupForm: NgForm;
+  subscription = "advanced";
+  password = '';
+  submitted = false;
+  user = {
+    email: '',
+    subscription: 'advanced',
+    password: ''
+  };
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log(this.signupForm.value.email);
+    this.submitted = true;
+    this.user.email = this.signupForm.value.email;
+    this.user.subscription = this.signupForm.value.subscription;
+    this.user.password = this.signupForm.value.password;
+
+    this.signupForm.reset();
   }
 
 }
