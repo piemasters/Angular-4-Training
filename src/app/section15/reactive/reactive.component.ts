fca9a345d20909abafbde70352eb53a2c49bcf9d
David <david.norton@cgi.com>
2017-09-04 10:24:33 +0100

    193. Reactive: Grouping Controls

--- src/app/section15/reactive/reactive.component.ts
+++ src/app/section15/reactive/reactive.component.ts
@@ -14,8 +14,10 @@ export class ReactiveComponent implements OnInit {
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      'username': new FormControl(null, Validators.required),
-      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+      }),
       'gender': new FormControl('male')
     });
   }
