afc03eabdb95cf19629cee080f9c96540d5e6b9d
David <david.norton@cgi.com>
2017-09-08 15:46:30 +0100

    211. Syncing HTML with the Form

--- src/app/shopping/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -55,7 +55,10 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+   
   }
 
 }
