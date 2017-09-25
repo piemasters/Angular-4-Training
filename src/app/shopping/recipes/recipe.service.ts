83af061edec8056382d91aa2c072839a0b37c9f1
David <david.norton@cgi.com>
2017-09-25 11:41:11 +0100

    Section: 19 Course Project - Http Completed

--- src/app/shopping/recipes/recipe.service.ts
+++ src/app/shopping/recipes/recipe.service.ts
@@ -31,6 +31,11 @@ export class RecipeService {
 
   constructor(private shoppingListService: ShoppingListService) { }
 
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
   // The empty slice returns a copy rather than original array
   getRecipes() {
     return this.recipes.slice();
--- src/app/shopping/shared/data-storage.service.ts
+++ src/app/shopping/shared/data-storage.service.ts
@@ -1,4 +1,5 @@
-import { Http } from '@angular/http';
+import { Recipe } from '../recipes/recipe.model';
+import { Http, Response } from '@angular/http';
 import { RecipeService } from '../recipes/recipe.service';
 
 import { Injectable } from '@angular/core';
@@ -9,5 +10,25 @@ export class DataStorageService {
 
     storeRecipes() {
         return this.http.put('https://ng-recipe-book-3a818.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+    }
+
+    getRecipes() {
+        this.http.get('https://ng-recipe-book-3a818.firebaseio.com/recipes.json')
+        .map(
+            (response: Response) => {
+                const recipes: Recipe[] = response.json();
+                for (const recipe of recipes) {
+                    if (!recipe['ingredients']) {
+                        recipe['ingredients'] = [];
+                    }
+                }
+                return recipes;
+            }
+        )
+        .subscribe(
+            (recipes: Recipe[]) => {
+                this.recipeService.setRecipes(recipes);
+            }
+        );
     }
 }
