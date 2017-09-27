65e1f3d5cd7da5e78b1194bbbf9f350f2f630d45
David <david.norton@cgi.com>
2017-09-27 10:13:50 +0100

    254. Sending the Token

--- src/app/shopping/shared/data-storage.service.ts
+++ src/app/shopping/shared/data-storage.service.ts
@@ -1,3 +1,4 @@
+import { AuthenticateService } from '../auth/auth.service';
 import { Recipe } from '../recipes/recipe.model';
 import { Http, Response } from '@angular/http';
 import { RecipeService } from '../recipes/recipe.service';
@@ -6,15 +7,17 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http, private recipeService: RecipeService) {}
+    constructor(private http: Http, private recipeService: RecipeService, private authService: AuthenticateService) { }
 
     storeRecipes() {
-        return this.http.put('https://ng-recipe-book-3a818.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+        const token = this.authService.getToken();
+        return this.http.put('https://ng-recipe-book-3a818.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
     }
 
     getRecipes() {
-        this.http.get('https://ng-recipe-book-3a818.firebaseio.com/recipes.json')
-        .map(
+        const token = this.authService.getToken();
+        this.http.get('https://ng-recipe-book-3a818.firebaseio.com/recipes.json?auth=' + token)
+            .map(
             (response: Response) => {
                 const recipes: Recipe[] = response.json();
                 for (const recipe of recipes) {
@@ -24,11 +27,11 @@ export class DataStorageService {
                 }
                 return recipes;
             }
-        )
-        .subscribe(
+            )
+            .subscribe(
             (recipes: Recipe[]) => {
                 this.recipeService.setRecipes(recipes);
             }
-        );
+            );
     }
 }
