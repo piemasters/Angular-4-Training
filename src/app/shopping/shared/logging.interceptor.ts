09bb904f49ed7012d7760a379967dd235dfdd02b
David <david.norton@cgi.com>
2017-10-03 11:58:41 +0100

    Section 23 Complete

--- src/app/shopping/shared/logging.interceptor.ts
+++ src/app/shopping/shared/logging.interceptor.ts
@@ -2,15 +2,16 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/c
 import {Observable} from 'rxjs/Observable';
 import {Injectable} from '@angular/core';
 import {AuthenticateService} from '../auth/auth.service';
+import 'rxjs/add/operator/do';
 
 @Injectable()
-export class AuthInterceptor implements HttpInterceptor {
+export class LoggingInterceptor implements HttpInterceptor {
   constructor(private authService: AuthenticateService) {}
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log('Intercepted!', req)
-    // const copiedReq = req.clone({headers: req.headers.append('', '')});
-    const copiedReq = req.clone({params: req.params.set('auth', this.authService.getToken())});
-    // console.log('Intercepted!', copiedReq)
-    return next.handle(copiedReq);
+    return next.handle(req).do(
+      event => {
+        console.log('Logging interceptor', event);
+      }
+    );
   }
 }
