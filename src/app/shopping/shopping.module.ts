454c38f6e87dc5c15112aee36818e90dc36b7621
David <david.norton@cgi.com>
2017-10-04 10:45:02 +0100

    316. Adjusting the App Module Setup

--- src/app/shopping/shopping.module.ts
+++ src/app/shopping/shopping.module.ts
@@ -8,7 +8,7 @@ import {AuthenticateModule} from './auth/auth.module';
 import {CoreModule} from './core/core.module';
 import {RecipesModule} from './recipes/recipe.module';
 import {StoreModule} from '@ngrx/store';
-import {shoppingListReducer} from "./shopping-list/store/shopping-list.reducers";
+import { reducers } from './store/shopping.reducers';
 
 @NgModule({
   declarations: [
@@ -21,7 +21,7 @@ import {shoppingListReducer} from "./shopping-list/store/shopping-list.reducers"
     AuthenticateModule,
     CoreModule,
     RecipesModule,
-    StoreModule.forRoot({shoppingList: shoppingListReducer})
+    StoreModule.forRoot(reducers)
   ],
   bootstrap: [ShoppingComponent]
 })
