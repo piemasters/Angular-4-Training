532da710232acebaffd2220ec7e1ba21f5e8085d
David <david.norton@cgi.com>
2017-10-27 16:55:03 +0100

    356. Animations Triggers and State

--- src/app/section26/animations/animations.component.ts
+++ src/app/section26/animations/animations.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {state, style, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-animations',
   templateUrl: './animations.component.html',
-  styleUrls: ['./animations.component.css']
+  styleUrls: ['./animations.component.css'],
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px)'
+      }))
+    ])
+  ]
 })
 export class AnimationsComponent implements OnInit {
+  state = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   constructor() { }
@@ -19,6 +33,14 @@ export class AnimationsComponent implements OnInit {
 
   onDelete(item) {
     this.list.splice(this.list.indexOf(item), 1);
+  }
+
+  onAnimate() {
+    console.log('test');
+  }
+
+  onShrink() {
+    console.log('test');
   }
 
 }
