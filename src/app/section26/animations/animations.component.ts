8501469575428c9671fa70c953dfd9491bfef22b
David <david.norton@cgi.com>
2017-12-18 17:15:25 +0000

    Section: 26 Angular Animations Completed

--- src/app/section26/animations/animations.component.ts
+++ src/app/section26/animations/animations.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import {animate, group, keyframes, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-animations',
@@ -61,6 +61,47 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
         }))
       ])
     ]),
+    trigger('list2', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        animate(1000, keyframes([
+          style({
+            transform: 'translateX(-100px)',
+            opacity: 0,
+            offset: 0
+          }),
+          style({
+            transform: 'translateX(-50px)',
+            opacity: 0.5,
+            offset: 0.3
+          }),
+          style({
+            transform: 'translateX(-20px)',
+            opacity: 1,
+            offset: 0.8
+          }),
+          style({
+            transform: 'translateX(0px)',
+            opacity: 1,
+            offset: 1
+          })
+        ]))
+      ]),
+      transition('* => void', [
+        group([
+          animate(300, style({
+            color: 'red'
+          })),
+          animate(800, style({
+            transform: 'translateX(100px)',
+            opacity: 0
+          }))
+        ])
+      ])
+    ])
   ]
 })
 export class AnimationsComponent implements OnInit {
@@ -89,5 +130,13 @@ export class AnimationsComponent implements OnInit {
 
   onShrink() {
     this.wildState = 'shrunken';
+  }
+
+  animationStarted(event) {
+    console.log(event);
+  }
+
+  animationEnded(event) {
+    console.log(event);
   }
 }
